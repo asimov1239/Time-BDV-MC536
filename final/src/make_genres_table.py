57852476b2adff3418a3e962ad3d0363f81a8a99
Daniel Coimbra <60114688+DanCoimbra@users.noreply.github.com>
2021-11-22 15:04:19 -0300

    Adds code o create genres table CSV

---
+++ final/src/make_genres_table.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+file = open('ratings_titles_genres_boxoffice_metacritic.csv')
+lines = file.readlines()
+
+splitlines = list()
+for line in lines:
+	splitlines.append(line.split(','))
+
+df = pd.DataFrame(columns=['imdb_id', 'genre'])
+idx = 0
+for line in splitlines:
+	_id = line[0]
+	genres = line[5:-2]
+	for genre in genres:
+		df = df.append(pd.DataFrame({'imdb_id': _id, 'genre': genre}, index=[idx]))
+		idx += 1
+
+df.to_csv('genres_table.csv', index=False)
